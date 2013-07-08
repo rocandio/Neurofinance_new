e2dd82efc70e00830be86ff2ccfeb1c26ef39b06
Rocandio <juanrocandio@hotmail.com>
2013-07-08 12:44:08 -0700

    Video_Reuters

--- media/media_videos.php
+++ media/media_videos.php
@@ -6,29 +6,33 @@ $(document).ready(function(){
 	$(".feedflare").hide();
  });
 </script>
-<div style="width:805px;">
+
 	<?php
 		$i=0;
 		$xml = simplexml_load_file('http://feeds.reuters.com/reuters/USVideoBusiness?format=xml'); //loading the document
-		$items = $xml->channel->item; //gets the title of the document.		
+		$items = $xml->channel->item; //gets the title of the document.
+		echo "<article  class=\"block_topic_post\">";		
 		foreach($items as $item){
-			if($i<5){
+			if($i<10){
 					$i++;
 					?>
-					<div style="float:left; width:200px;">
+					<div style="float:left; width:150px;">
 						<a href="<?php echo $item->link;?>"><img src="<?php echo $item->children('media',true)->group->content->thumbnail->attributes()->url;?>"/></a>
 					</div>
-					<div style="float:left; width:600px; ">
+					<div style="float:left; width:400px; ">
 						<?php
-						echo "<a style=\"font-family: 'Roboto',sans-serif;font-size: 19px;text-transform:uppercase;color:#8B3A3A;font-style: bold;\"href=\"".$item->link."\" target=\"_blank\">".$item->title."</a>";
+						echo "<a class=\"links_news\" href=\"".$item->link."\" target=\"_blank\">".$item->title."</a>";
 						echo "</br>";
+						echo "<p class=\"p_news\">";
 						echo $item->description;
+						echo "</p>";
+						
+						echo "<p class=\"publ_news\"style=\"text-align:right\">".$item->pubDate."</p></br>";
 						echo "</br></br>";
-						echo "<p style=\"text-align:right\">".$item->pubDate."</p></br>";
 					echo "</div>";
 			}
 			else
 				break;
 		}
+		echo "</article>";
 	?>
-</div>
