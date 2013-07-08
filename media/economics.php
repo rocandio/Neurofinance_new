c0a0c4abf5d934714ebf74319b2455c925b73a48
Santos1 <global.santos1@gmail.com>
2013-07-08 12:51:50 -0700

    Media Tab
    Media Tab has working links

---
+++ media/economics.php
@@ -0,0 +1,29 @@
+<script>
+$(document).ready(function(){
+    $(".feedflare").hide();
+ });
+</script>
+    <?php
+        $i=0;
+        $xml = simplexml_load_file('http://www.bloomberg.com/tvradio/podcast/cat_economics.xml'); //loading the document
+        $items = $xml->channel->item; //gets the title of the document.     
+        echo "<article  class=\"block_topic_post\">";
+        foreach($items as $item){
+            if($i<10){
+                    $i++;
+                    ?>
+                    <?php
+                    echo "<a class=\"links_news\"href=\"".$item->link."\" target=\"_blank\">".$item->title."</a>";
+                    echo "</br>";
+                    echo "<p class=\"p_news\">";
+                    echo $item->children('itunes',true)->subtitle;
+                    echo "</p>";
+                    echo "<p class=\"publ_news\"style=\" style=\"text-align:right\">"."Duration: ".$item->children('itunes',true)->duration." PubDate: ".$item->pubDate."</p></br>";
+                    echo "</br></br>";
+
+            }
+            else
+                break;
+        }
+        echo "</article>";
+    ?>
